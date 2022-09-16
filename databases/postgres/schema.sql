d5e992de19c08221c544043869e429e170a7c62e
Nick <nicolastiennguyen@gmail.com>
2022-09-15 17:08:22 -0700

    successful injection of csv into db

--- databases/postgres/schema.sql
+++ databases/postgres/schema.sql
@@ -1,4 +1,22 @@
--- CREATE DATABASE products;
+-- DROP DATABASE IF EXISTS products
+
+CREATE DATABASE products;
+
+\c products
+
+CREATE TABLE features (
+id SERIAL PRIMARY KEY,
+product_id INTEGER NOT NULL,
+feature VARCHAR(100) NOT NULL,
+value VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE photos (
+id SERIAL PRIMARY KEY,
+styleId INTEGER NOT NULL,
+url TEXT NOT NULL,
+thumbnail_url TEXT NOT NULL
+);
 
 CREATE TABLE products (
   id SERIAL PRIMARY KEY,
@@ -15,32 +33,27 @@ CREATE TABLE related (
   related_product_id INTEGER NOT NULL
 );
 
-CREATE TABLE features (
+CREATE TABLE sku (
 id SERIAL PRIMARY KEY,
-product_id INTEGER NOT NULL,
-feature VARCHAR(100) NOT NULL,
-value VARCHAR(100) NOT NULL
-)
+styleId INTEGER NOT NULL,
+size VARCHAR(10) NOT NULL,
+quantity INTEGER NOT NULL
+);
 
 CREATE TABLE styles (
 id SERIAL PRIMARY KEY,
-product_id INTEGER NOT NULL,
+productId INTEGER NOT NULL,
 name VARCHAR(100) NOT NULL,
-sale_price INTEGER DEFAULT NULL,
+sale_price TEXT,
 original_price INTEGER NOT NULL,
-default_style BOOLEAN DEFAULT TRUE,
-)
+default_style INTEGER NOT NULL
+);
 
-CREATE TABLE sku (
-id SERIAL PRIMARY KEY,
-style_id INTEGER NOT NULL,
-size VARCHAR(10) NOT NULL
-quantity INTEGER NOT NULL,
-)
 
-CREATE TABLE photos (
-id SERIAL PRIMARY KEY,
-style_id INTEGER NOT NULL,
-url VARCHAR(1000) NOT NULL
-thumbnail_url VARCHAR(1000) NOT NULL,
-)
+
+COPY features FROM '/Users/nicolasnguyen/hackreactor2207/products-server/.csv/features.csv' DELIMITER ',' CSV HEADER;
+COPY photos FROM '/Users/nicolasnguyen/hackreactor2207/products-server/.csv/photos.csv' DELIMITER ',' CSV HEADER;
+COPY products FROM '/Users/nicolasnguyen/hackreactor2207/products-server/.csv/product.csv' DELIMITER ',' CSV HEADER;
+COPY related FROM '/Users/nicolasnguyen/hackreactor2207/products-server/.csv/related.csv' DELIMITER ',' CSV HEADER;
+COPY sku FROM '/Users/nicolasnguyen/hackreactor2207/products-server/.csv/skus.csv' DELIMITER ',' CSV HEADER;
+COPY styles FROM '/Users/nicolasnguyen/hackreactor2207/products-server/.csv/styles.csv' DELIMITER ',' CSV HEADER;
