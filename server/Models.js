0426b7441ec05b76886721b71726df3dd9a3d8ff
Nick <nicolastiennguyen@gmail.com>
2022-09-16 10:42:50 -0700

    added comments

--- server/Models.js
+++ server/Models.js
@@ -1,10 +1,11 @@
 const pool = require("./connection");
 
 module.exports = {
+  // returns a product's information
   readProduct: (product_id) => {
     return pool.connect()
       .then(client => {
-          const query = `SELECT name FROM products WHERE id = $1`;
+          const query = `SELECT name, slogan, description, category, default_price FROM products WHERE id = $1`;
           return client.query(query, [product_id])
             .then(res => {
               client.release()
@@ -17,10 +18,11 @@ module.exports = {
       })
   },
 
+  // returns a product's related IDs
   readRelated: (product_id) => {
     return pool.connect()
       .then(client => {
-        const query = `SELECT ARRAY_AGG (related.related_product_id) FROM related WHERE related.current_product_id = $1`
+        const query = `SELECT ARRAY_AGG(related_product_id) FROM related WHERE current_product_id = $1`
         return client.query(query, [product_id])
           .then(res => {
             client.release()
