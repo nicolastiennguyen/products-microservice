0aaa92f06d907cbfa86639d8af4199019f6021ad
Nick <nicolastiennguyen@gmail.com>
2022-09-22 09:32:51 -0700

    updated db connection

--- server/connection.js
+++ server/connection.js
@@ -2,11 +2,18 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env_DB_USER,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME
+  host: process.env.PGHOST,
+  user: process.env_PGUSER,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE
 });
+// const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   user: process.env_DB_USER,
+//   password: process.env.DB_PASS,
+//   port: process.env.DB_PORT,
+//   database: process.env.DB_NAME
+// });
 
 module.exports = pool;
